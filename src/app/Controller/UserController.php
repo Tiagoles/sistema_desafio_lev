2e95418d9cf72596e1942cb48177e3327c4aec6f
tiagoles <tiagodamascenogames@gmail.com>
2024-08-16 23:37:22 -0300

    quick fix

--- src/app/Controller/UserController.php
+++ src/app/Controller/UserController.php
@@ -2,9 +2,6 @@
   if (session_status() == PHP_SESSION_NONE) {
     session_start();
   }
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
   require "../Model/User.php";
   $userModel = new User();
 
--- src/app/Model/User.php
+++ src/app/Model/User.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 require "/home/tiagoles/Documents/sistema_desafio_lev/config/db.php";
 
 class User
