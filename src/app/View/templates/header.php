5a343fa6a2bfd0cbaea76063a488a44c150cb428
tiagoles <tiagodamascenogames@gmail.com>
2024-08-22 18:38:35 -0300

    Style: Ajustes necessários no css para uma melhor visualização

--- src/app/View/templates/header.php
+++ src/app/View/templates/header.php
@@ -9,6 +9,8 @@
     } elseif ($currentPage == 'list.php') {
         $pageTitle = "Visualizar";
     }
+
+    $baseUrl = "/home/tiagoles/Documents/sistema_desafio_lev/"
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -18,20 +20,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?=$pageTitle?></title>
         <link rel="stylesheet" href="../../../../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../../../../vendor/twbs/bootstrap/dist/js/bootstrap.min.js">
-        <link rel="stylesheet" href="../../../../vendor/twbs/bootstrap/dist/js/">
+        <script src="../../../../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="/Public/css/Register.css">
-        <link rel="stylesheet" href="/Public/css/List.css">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-        <script src="/Public/js/maskPhoneNumber.js"></script>
+        <script ></script>
     </head>
 
     <body class="bg-secondary-subtle">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link icon-link icon-link-hover link-success link-underline-success link-underline-opacity-25 fw-semibold" href="register.php">Cadastrar</a>
+                    <a class="nav-link icon-link icon-link-hover link-success link-underline-success link-underline-opacity-25 fw-semibold" href="index.php">Cadastrar</a>
                     <a class="nav-link icon-link icon-link-hover link-success link-underline-success link-underline-opacity-25 fw-semibold" href="list.php">Visualizar</a>
                 </div>
             </div>
@@ -39,4 +38,6 @@
         <main>
             <h1 class="text-center py-5"><?= $pageTitle ?></h1>
             <div class="container mt-5">
+
+     
     
